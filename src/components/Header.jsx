4c145af2e6f49d2f6100271a6f348dd64df20d0e
Krishrx <kalikrishnas342@gmail.com>
2023-10-04 00:45:34 +0530

    added hover effect for theme icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,8 +7,8 @@ function Header() {
   return (
     <div className={`w-10/12 px-2 py-4 flex justify-between items-center ${isDark?'text-gray-100':''}`}>
       <h1 className='font-bold text-2xl font-Caudex'>WCD</h1>
-      <div className="cursor-pointer" onClick={toggleTheme}>
-        {isDark?<Sun color="#fcd34d"/>:<Moon fill="#030712" />}
+      <div className={`cursor-pointer p-3 rounded-full ${isDark?'bg-gray-300 hover:bg-gray-200':'bg-gray-200 hover:bg-gray-300'}`} onClick={toggleTheme}>
+        {isDark?<Sun color="#eab308"/>:<Moon  />}
       </div>
     </div>
   );
